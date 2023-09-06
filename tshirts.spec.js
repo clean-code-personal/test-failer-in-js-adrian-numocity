f41451603f3fa79a37294686487b8601cdce6bc0
adrian-numocity <adrian.p@numocity.com>
2023-09-06 18:17:02 +0530

    Test2

---
+++ tshirts.spec.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+const tshirts = require('./tshirts');
+
+describe('size function', () => {
+    it('should return an error for invalid input', () => {
+        // Test with an invalid input that should trigger an error
+        expect(() => tshirts('invalid')).to.throw(Error);
+    });
+
+    it('should correctly classify sizes', () => {
+        // Test with valid inputs
+        expect(tshirts(37)).to.equal('S');
+        expect(tshirts(40)).to.equal('M');
+        expect(tshirts(43)).to.equal('L');
+        expect(() => tshirts(-20)).to.throw(Error,'Shoulder size cannot be negative');
+        expect(()=>tshirts(38)).to.equal('M');
+        expect(()=>tshirts(100)).to.throw(Error,'Shoulder size exceeds limit')
+    });
+});
